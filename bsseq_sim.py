0f793ab645651f9778608a7441a156ba3d04c0c7
dangertrip <dangertrip@hotmail.com>
2019-09-19 14:18:01 -0500

    simulation

--- bsseq_sim.py
+++ bsseq_sim.py
@@ -5,158 +5,154 @@ import sys
 
 #==============================Variable setting===================================
 readsname = sys.argv[1]
+readsnum= int(sys.argv[2])
 conversion_ratio=1
 methylation_ratio=0.75
-readsnum=5000000
-readlength=80
+read_length=100
+chrom_set, chrom_len, chrom_name, genome_len = genome_loader('/data/dsun/ref/humanigenome/hg19.fa')
+methy_dict = bed_reader('./hESC.bed')
+'''
+@Variables:
+chrom_set: dictionary, {chromsome_name:chromsome_genome}
+chrom_len: int[], chromsome length ordered by chromsome name in fastq file
+chrom_name: string[], chromsome names
+genome_len: int, length of whole genome
+methy_dict: dictionary, {'chr1_10469':0.1(methylation ratio)}
+'''
 #================================================================================
 
+def bed_reader(filename):
+    '''
+        read bed file as fixed methylation ratio
+    '''
+    dict = {}
+    with open(filename) as f:
+        for line in f:
+            line_content = line.strip().split()
+            dict[line_content[0]+'_'+line_content[2]] = float(line_content[4])
+    return dict
 
-#==============================Get Genome fasta dictory=========================
-chr=''
-ans={}
-pos=0
-mark=False
-s=''
-length=0
-l=[]
-chr_order=[]
-with open('/data/dsun/ref/humanigenome/hg19.fa') as f:
-    for line in f:
-        temp = line.strip()
-        if temp[0]=='>':
-            if chr!='':
-                ans[chr]=s
-                ll=len(s)
-                length+=ll
-                l.append(ll)
-                s=''
-            chr= temp[1:]
-            chr_order.append(chr)
-            continue
-        s+=temp
-ans[chr]=s
-ll=len(s)
-l.append(ll)
-length+=ll
+def genome_loader(filename):
+    chr = ''
+    chrom_set = {}
+    seq = ''
+    genome_len = 0
+    chrom_len = []
+    chrom_name = []
+    with open(filename) as f:
+        for line in f:
+            line_content = line.strip()
+            if line_content[0]=='>':
+                if chr!='':
+                    chrom_set[chr] = seq
+                    chrom_len.append(len(seq))
+                    genome_len += chrom_len[-1]
+                    seq=''
+                chr = line_content[1:]
+                chrom_name.append(chr)
+                continue
+            seq += line_content
+        chrom_set[chr] = seq
+        chrom_len.append(len(seq))
+        genome_len += chrom_len[-1]
+    return chrom_set, chrom_len, chrom_name, genome_len
 
 
+def fake_read(length):
 
-#print(ans['chr3'][43483166:43483246].upper())
-#print(ans['chr3'][43483379:43483459].upper())
-#sys.exit()
-#=========================================================================
-
-'''
-def getcg(read):
-    num=0
-    if random.random()>0.5:
-        for i in range(1,len(read)):
-            if ('G'==read[i] or 'g'==read[i]) and ('C'==read[i-1] or 'c'==read[i-1]):
-                num+=1
-    else:
-        for i in range(1,len(read)):
-            if ('C'==read[i] or 'c'==read[i]) and ('G'==read[i-1] or 'g'==read[i-1]):
-                num+=1
-
-    return num
-'''
-
-def fake(fl,read):
-    base=['A','T','C','G']
-    l = len(read)
-    if l>=fl:
-        return read,0,0
-    front = int((fl - l)/2)
-    end = fl-l-front
-    r=read
-    for i in range(front):
+    base = ['A','T','C','G']
+    seq = ''
+    for i in range(length):
         pos = random.randint(0,3)
-        r=base[pos]+r
-    for i in range(end):
-        pos = random.randint(0,3)
-        r=r+base[pos]
-    #print(r,front,end,l)
-    return r,front,end
+        seq += base[pos]
+    return seq
+
 
 def reverse(read):
     dic={'A':'T','T':'A','C':'G','G':'C','N':'N'}
     r=''
-#    print('r')
     for rr in read:
         r=dic[rr.upper()]+r
     return r
 
-def bisulfite(conv_r,meth_r,read):
+
+def bisulfite(chrom, start, read):
+    '''
+    return bisulfited seq.
+    Ignore the reads if get ''
+    '''
     r=''
     l = len(read)
-    for i in range(1,l-1):
+    for i in range(1,l-1): #waste 2 bp
         base=read[i].upper()
         if base=='C':
             c = random.random()
-            if c<conv_r:
+            if c<conversion_ratio:
                 if read[i+1].upper()=='G':
+                    pos = chrom+'_'+str(start+i)
+                    if pos not in methy_dict:
+                        return ''
                     m = random.random()
-                    if m>meth_r:
+                    if m>methy_dict[pos]:
                         base='T'
                 else:
                     base='T'
         r=r+base
     return r
 
-#time1=time.time()
-num=[]
-zero=0
-finalbed=[]
-for i in range(readsnum):
-    pos = random.randint(0,length)
-    chr=0
-    f=0
-    t=0
-    while pos>l[chr]:
-        pos-=l[chr]
-        chr+=1
-    #readlength=length
-    start=pos-1
-    end=pos+readlength+1
-    if pos<1:continue
-    if pos>l[chr]:continue
-    read=ans[chr_order[chr]][start:end]
-    r=read
-    a1=random.random()
-    a2=random.random()
-    if a1>0.5:
-        r=reverse(read)#Get reads from +/- strand
-    r = bisulfite(conversion_ratio,methylation_ratio,r)
-    if a2>0.5:
-        r=reverse(r)#PCR +/-
-    #r,f,t = fake(fake_length,r)
-    quality = 'E'*readlength
-#    print(readlength)
-    #print(a1,a2)
-    #print(read)
-    print('@'+str(i)+'_'+readsname)
-    print(r)
-    print('+')
-    print(quality)
-    finalbed.append(chr_order[chr]+'\t'+str(start+1)+'\t'+str(end-1)+'\t'+str(i)+'_'+readsname+'\t'+str(f)+'\t'+str(t)+'\n')
-with open(readsname+'_simulation.bed','w') as f:
-    f.writelines(finalbed)
-
-#print(time.time()-time1)
-    #g=getcg(read)
-    #num.append(g)
-    #if g==0: zero+=1
-#print(zero/float(100000000))
-#print((100000000-zero)/float(100000000))
-
-#import seaborn as sns
-#from matplotlib import pyplot as plt
-#import numpy as np
-#n=np.array_split(num,10)
-#for nn in n:
-#    sns.distplot(nn)
-#plt.savefig('125.pdf')
 
 
-##
+'''
+@Variables:
+chrom_set: dictionary, {chromsome_name:chromsome_genome}
+chrom_len: int[], chromsome length ordered by chromsome name in fastq file
+chrom_name: string[], chromsome names
+genome_len: int, length of whole genome
+methy_dict: dictionary, {'chr1_10469':0.1(methylation ratio)}
+'''
+
+def random_head_tail():
+    finalbed=[]
+    fake_length = 20
+    real_read_length = read_length - fake_length
+    for i in range(readsnum):
+        pos = random.randint(0,genome_len)
+        chr=0
+        real_read_length = read_length - 20
+        while pos>chrom_len[chr]:
+            pos-=chrom_len[chr]
+            chr+=1
+        start=pos-1
+        end=pos+real_read_length+1
+        if pos<1:continue
+        if pos>chrom_len[chr]:continue
+        read=chrom_set[chrom_name[chr]][start:end]
+        r=read
+        a1=random.random()
+        a2=random.random()
+        if a1>0.5:
+            r=reverse(read)#Get reads from +/- strand
+        r = bisulfite(conversion_ratio,methylation_ratio,r)
+        if a2>0.5:
+            r=reverse(r)#PCR +/-
+        fake_marker=''
+        if fake_length>0:
+            head_fake_length = random.randint(0,fake_length)
+            tail_fake_length = fake_length - head_fake_length
+            head = fake_read(head_fake_length)
+            tail = fake_read(tail_fake_length)
+            r = head + r + tail
+            fake_marker = '_'+str(head_fake_length)+'_'+str(tail_fake_length)
+        quality = 'E'*read_length
+        print('@'+str(i)+'_'+readsname)
+        print(r)
+        print('+')
+        print(quality)
+        finalbed.append(chrom_name[chr]+'\t'+str(start+1)+'\t'+str(end-1)+'\t'+str(i)+'_'+readsname+'\t'+str(f)+'\t'+str(t)+'\n')
+    with open(readsname+'_simulation.bed','w') as f:
+        f.writelines(finalbed)
+
+
+if __name__=="__main__":
+    random_head_tail()
+    
