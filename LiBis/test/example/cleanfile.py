31b4f73d076e32841ac54e8adbf80e3846db00b7
dangertrip <dangertrip@hotmail.com>
2020-03-03 16:49:05 -0600

    Examples

--- LiBis/test/example/cleanfile.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('rm -rf BED_FILE')
-os.system('rm -rf BAM_FILE')
-os.system('rm -rf Fastqc')
-os.system('rm -rf Trim')
-os.system('rm -rf RESULT')
-os.system('rm -rf *.mapreduce')
